52b53b0eb048cec7e1b17f6233befee2f22b67ff
Matheus Martins <matheushmar@gmail.com>
2023-08-29 10:27:33 -0300

    feat: 🎸 Add new tests

--- app.vue
+++ app.vue
@@ -1,5 +1,68 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="container">
+    <h1 class="container__title">Project to train tests with Vitest</h1>
+    <p class="container__result">{{ count }}</p>
+    <div class="container__buttons">
+      <button class="addition" @click="count = count + 1">+</button>
+      <button class="subtraction" @click="count = count - 1">-</button>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const count: Ref<number> = ref(0)
+</script>
+
+<style scoped lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  display: block;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  padding: 3rem;
+  border: 2px solid blueviolet;
+  border-radius: 1rem;
+
+  &__title {
+    margin-bottom: 3rem;
+    text-transform: uppercase;
+  }
+
+  &__result {
+    padding: 1rem;
+    border: 1px solid black;
+    margin-bottom: 2rem;
+    border-radius: 8px;
+    width: 8.5rem;
+    text-align: center;
+    background-color: rgba(137, 43, 226, 0.1);
+    font-size: 3rem;
+    font-weight: 900;
+  }
+
+  &__buttons {
+    display: flex;
+    gap: 1rem;
+
+    button {
+      width: 5rem;
+      height: 4rem;
+      background-color: blueviolet;
+      color: white;
+      font-size: 2rem;
+      font-weight: 700;
+      border: 1px solid black;
+      border-radius: 8px;
+    }
+  }
+}
+</style>
---
+++ tests/basic.test.ts
@@ -0,0 +1,65 @@
+import { fileURLToPath } from 'node:url'
+import { describe, expect, it } from 'vitest'
+import { setup, $fetch, isDev } from '@nuxt/test-utils'
+import { mount } from '@vue/test-utils'
+
+// imports
+import App from  './app.vue'
+
+// describe('example', async () => {
+//   await setup({
+//     rootDir: fileURLToPath(new URL('..', import.meta.url)),
+//     server: true
+//   })
+
+//   it('Renders Hello Nuxt', async () => {
+//     expect(await $fetch('/')).toMatch('Hello Nuxt!')
+//   })
+
+//   if (isDev()) {
+//     it('[dev] ensure vite client script is added', async () => {
+//       expect(await $fetch('/')).toMatch('/_nuxt/@vite/client"')
+//     })
+//   }
+// })
+
+describe('Bloco de testes', async() => {
+  // await setup({
+  //   rootDir: fileURLToPath(new URL('..', import.meta.url)),
+  //   server: true
+  // })
+
+  it('Testa texto do título', () => {
+    const wrapper = mount(App)
+    expect(wrapper.find('.container__title').html().includes('</h1>')).toBe(true)
+    expect(wrapper.find('.container__title').text()).toEqual('Project to train tests with Vitest')
+  })
+
+  it('Testa botão de adição', () => {
+    const wrapper = mount(App)
+    expect(wrapper.find('.addition').text()).toEqual('+')
+  })
+
+  it('Testa botão de subtratação', () => {
+    const wrapper = mount(App)
+    expect(wrapper.find('.subtraction').text()).toEqual('-')
+  })
+
+  it('Testa visualização do input ao clicar botão de adição', async () => {
+    const wrapper = mount(App)
+    const input = wrapper.find('.container__result')
+    expect(input.text()).toBe('0')
+    const buttonAdd = wrapper.find('.addition')
+    await buttonAdd.trigger('click')
+    expect(input.text()).toBe('1')
+  })
+
+  it('Testa visualização do input ao clicar botão de subtração', async () => {
+    const wrapper = mount(App)
+    const input = wrapper.find('.container__result')
+    expect(input.text()).toBe('0')
+    const buttonSub = wrapper.find('.subtraction')
+    await buttonSub.trigger('click')
+    expect(input.text()).toBe('-1')
+  })
+})
